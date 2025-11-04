f5bfe516e7904c9e35afc4a6a13cc128bbf0578b
Max <maxsiepsiak238@gmail.com>
2025-11-04 15:20:02 +0100

    Init h1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,31 @@
+package h1;
+
+public class H1_main {
+    public static void main(String[] args) {
+        double guthaben = 12, monEingang = 23;
+        int rating = 4;
+        boolean warnhinweis = true, negativ = false;
+
+        if (guthaben < 0) {
+            negativ = true;
+            if (Math.abs(monEingang) >= Math.abs(guthaben)) {
+                rating++;
+            } else {
+                rating--;
+                if (rating < 0) {
+                    warnhinweis = true;
+                } else {
+                    warnhinweis = false;
+                }
+            }
+        } else if (guthaben == 0) {
+            rating += 2;
+            negativ = false;
+        } else {
+            rating += 3;
+            negativ = false;
+        }
+
+
+    }
+}
