f63baafdf43ecd7009215dd50e9d11432f0feffd
Max <maxsiepsiak238@gmail.com>
2025-11-05 14:06:00 +0100

    2. Commit h2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -2,6 +2,18 @@ package h2;
 
 public class H2_main {
     public static void main(String[] args) {
-        int i = 10;
+        int jahr = 2016;
+        boolean schalt = false;
+
+        if(jahr % 400 == 0) {
+            schalt = true;
+        }else {
+            if(jahr % 100 != 0) {
+                if(jahr % 4 == 0) {
+                    schalt = true;
+                }
+            }
+        }
+        System.out.println(schalt);
     }
 }
